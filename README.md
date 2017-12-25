b97b58546161fa38eafaed36e0027058b2f0ff51
COWELL\yiming.hsu <yiming.hsu@cowell.com.tw>
2017-12-25 10:53:30 +0800

    done

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # serialize-json
  Serialize form data to Json (deep structure).
+
+serialises a form to an object. The use is the same with $.fn.serialize
+and $.fn.serializeArray, or serializeJson(array)
+The keys are the form element names(split) and the value is the the form element
+value. If multiple form elements have the same name with different values,
+the value will replace the origin value.
+
+# usage
+
+```
+var jQuery = require('jquery');
+// or 
+//   <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+var arr = [
+    {"name":"desc", "value":"demo serializeArray of form to json object."},
+    {"name":"users[0].name", "value":"user A"},
+    {"name":"users[0].skills[0].name", "value":"PHP"},
+    {"name":"users[0].skills[0].years", "value":"12"},
+    {"name":"users[0].skills[1].name", "value":"Ruby"},
+    {"name":"users[0].skills[1].years", "value":"5"},
+    {"name":"users[0].skills[2].name", "value":"C#"},
+    {"name":"users[0].skills[2].years", "value":"2"},
+    {"name":"users[1].name", "value":"user B"},
+    {"name":"users[1].skills[0].name", "value":"Docker"},
+    {"name":"users[1].skills[0].years", "value":"5"},
+    {"name":"users[1].skills[1].name", "value":"AWS"},
+    {"name":"users[1].skills[1].years", "value":"7"},
+    {"name":"data.replacement", "value": "origin value"},
+    {"name":"data.replacement", "value": "replace value"}
+];
+
+var output = serializeJson(arr); // or  $('#form').serializeJson());
+
+/** 
+output: {
+    desc: "demo serializeArray of form to json object.",
+    users: [
+        {
+            name: "user A",
+            skills: [
+                { name: "PHP", years: 12 },
+                { name: "Ruby", years: 5 },
+                { name: "C#", years: 2 }
+            ]
+        },
+        {
+            name: "user B",
+            skills: [
+                { name: "Docker", years: 5 },
+                { name: "AWS", years: 7 }
+            ]
+        }
+    ],
+    data: {
+        replacement: 'replace value'
+    }
+}
+*/
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+/**
+MIT License
+
+Copyright (c) 2017 yiming.hsu <mufasa.hsu@gmail.com}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+serialises a form to an object. The use is the same with $.fn.serialize
+and $.fn.serializeArray, or serializeJson(array)
+The keys are the form element names(split) and the value is the the form element
+value. If multiple form elements have the same name with different values,
+the value will replace the origin value.
+*/
+
+// main function
+function serializeJson (formArr) {
+    var data = {};
+    if(formArr instanceof Array) {
+        data = formArr.reduce(function (r, o) {
+            var s = r, arr = o.name.split('.');
+            arr.forEach((n, k) => {
+                var ck = n.replace(/\[[0-9]*\]$/, "");
+                if (!s.hasOwnProperty(ck))
+                    s[ck] = (new RegExp("\[[0-9]*\]$").test(n)) ? [] : {};
+                if (s[ck] instanceof Array) {
+                    var i = parseInt((n.match(new RegExp("([0-9]+)\]$")) || []).pop(), 10);
+                    i = isNaN(i) ? s[ck].length : i;
+                    s[ck][i] = s[ck][i] || {};
+                    (k === arr.length - 1) ? s[ck][i] = o.value : s = s[ck][i];
+                }
+                else {
+                    (k === arr.length - 1) ? s[ck] = o.value : s = s[ck];
+                }
+            });
+            return r;
+        }, {});
+    }
+    return data;
+}
+
+// for jquery
+if(typeof jQuery !== "undefined") {
+    jQuery.fn.extend({
+        serializeJson: function() {
+            return serializeJson( this.serializeArray() );
+        }
+    });
+}
+
+// for nodejs
+if(typeof module !== "undefined") {
+    module.exports = serializeJson;
+}
